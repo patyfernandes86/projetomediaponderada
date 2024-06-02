fd9bc67f8c13aa00c13a3712d6babde5e2d063d4
patyfernandes86 <111815919+patyfernandes86@users.noreply.github.com>
2024-06-02 11:56:13 -0300

    Add files via upload

---
+++ trabalho-1.js
@@ -0,0 +1,10 @@
+
+var n1 = 22
+var n2 = 36
+var n3 = 79
+var p1 = 3
+var p2 = 9
+var p3 = 5
+
+var media = (n1 * p1 + n2 * p2 + n3 * p3) / (p1 + p2 + p3)
+console.log("A média ponderada é: " + media)
